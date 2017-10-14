1b6acab94331c9a3254e8d8b5dbab2986fbf6f82
shawnyan <shawn2016@aliyun.com>
2017-10-14 23:47:11 +0800

    wsshost

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 App({
     config: {
       host: '97374127.shawnyan.xyz',
-      wsshost: '97374127.ws.qcloud.la'
+      //wsshost: '97374127.ws.qcloud.la'
+      wsshost: '97374127.shawnyan.xyz'
     },
     onLaunch () {
         console.log('App.onLaunch()');
